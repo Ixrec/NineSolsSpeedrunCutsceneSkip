93f557b256aa5a7356bfbe5d5794c12e4c43f096
Ixrec <ericrhitchcock@gmail.com>
2025-10-05 21:17:49 +0100

    add a file for the post-LE video skip

---
+++ Source/FullPath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpeedrunCutsceneSkip;
+
+internal class FullPath
+{
+    public static string GetFullPath(GameObject go)
+    {
+        var transform = go.transform;
+        List<string> pathParts = new List<string>();
+        while (transform != null)
+        {
+            pathParts.Add(transform.name);
+            transform = transform.parent;
+        }
+        pathParts.Reverse();
+        return string.Join("/", pathParts);
+    }
+}
---
+++ Source/LadyEtherealPostBoss.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+
+namespace SpeedrunCutsceneSkip;
+
+[HarmonyPatch]
+internal class LadyEtherealPostBoss
+{
+    [HarmonyPrefix, HarmonyPatch(typeof(VideoPlayAction), "OnStateEnterImplement")]
+    private static void VideoPlayAction_OnStateEnterImplement(VideoPlayAction __instance)
+    {
+        if (__instance.name == "[Action] VideoPlayAction")
+        {
+            var goPath = FullPath.GetFullPath(__instance.gameObject);
+            if (goPath == "GameLevel/Room/Prefab/SimpleCutSceneFSM_結局_大爆炸/--[States]/FSM/[State] PlayCutSceneEnd/[Action] VideoPlayAction")
+            {
+                Log.Info($"SpeedrunCutsceneSkip skipping the post-Lady Ethereal Heng flashback video");
+                AccessTools.Method(typeof(VideoPlayAction), "TrySkip").Invoke(__instance, []);
+            }
+        }
+    }
+}
