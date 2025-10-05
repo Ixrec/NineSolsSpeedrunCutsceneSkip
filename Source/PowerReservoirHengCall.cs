29afc62e84ef6b34c2c7126cc6c3cabb29384a31
Ixrec <ericrhitchcock@gmail.com>
2025-10-05 21:23:48 +0100

    also add a file for the PR Heng skip

---
+++ Source/PowerReservoirHengCall.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace SpeedrunCutsceneSkip;
+
+[HarmonyPatch]
+internal class PowerReservoirHengCall
+{
+    // The Heng flashback in Power Reservoir got its own special implementation class instead of using SimpleCutsceneManager
+    [HarmonyPrefix, HarmonyPatch(typeof(A2_SG4_Logic), "EnterLevelStart")]
+    private static void A2_SG4_Logic_EnterLevelStart(A2_SG4_Logic __instance)
+    {
+        Log.Info($"SpeedrunCutsceneSkip skipping the Power Reservoir (Center) Heng call");
+        __instance.TrySkip();
+    }
+}
