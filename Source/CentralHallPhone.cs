0166bb12879b21210de2640e39b11d1b8f1c4d95
Juanba <bauti777@gmail.com>
2025-11-26 00:45:29 -0300

    add: pavilion and central hall intro cutscene skips

---
+++ Source/CentralHallPhone.cs
@@ -0,0 +1,28 @@
+﻿using Cysharp.Threading.Tasks;
+using HarmonyLib;
+
+namespace SpeedrunCutsceneSkip;
+
+[HarmonyPatch]
+internal class CentralHallPhone
+{
+    [HarmonyPostfix, HarmonyPatch(typeof(SimpleCutsceneManager), "PlayAnimation")]
+    private static async void SimpleCutsceneManager_PlayAnimation_Postfix(SimpleCutsceneManager __instance)
+    {
+        if (!SpeedrunCutsceneSkip.Instance.SkipSetting.Value)
+            return;
+        if (__instance.transform.parent.parent.parent.name == "SimpleCutSceneFSM_Tree intro")
+        {
+            var goPath = FullPath.GetFullPath(__instance.gameObject);
+            if (goPath == "AG_S1/Room/SimpleCutSceneFSM_Tree intro/FSM Animator/LogicRoot/[CutScene]")
+            {
+                Log.Info($"SpeedrunCutsceneSkip waiting 100 frames before skipping the Warehouse Heng call");
+
+                await UniTask.DelayFrame(100);
+
+                Log.Info($"SpeedrunCutsceneSkip skipping the Senate Hall call");
+                __instance.TrySkip();
+            }
+        }
+    }
+}
---
+++ Source/PavillionNymphIntro.cs
@@ -0,0 +1,42 @@
+﻿using Cysharp.Threading.Tasks;
+using HarmonyLib;
+
+namespace SpeedrunCutsceneSkip;
+
+[HarmonyPatch]
+internal class PavillionNymphIntro
+{
+    [HarmonyPostfix, HarmonyPatch(typeof(SimpleCutsceneManager), "PlayAnimation")]
+    private static async void SimpleCutsceneManager_PlayAnimation_Postfix(SimpleCutsceneManager __instance)
+    {
+        if (!SpeedrunCutsceneSkip.Instance.SkipSetting.Value)
+            return;
+
+        if (__instance.transform.parent.parent.parent.parent.name == "NPC_AICore_FSM")
+        {
+            var goPath = FullPath.GetFullPath(__instance.gameObject);
+            if (goPath == "AG_S2/Room/Prefab/ControlRoom FSM Binding Tool/NPC_AICore_Base/NPC_AICore_Base_FSM/FSM Animator/LogicRoot/NPC_AICore_FSM/General FSM Object/Animator(FSM)/LogicRoot/[CutScene]初次對話演出")
+            {
+                Log.Info($"SpeedrunCutsceneSkip waiting 100 frames before skipping the Ruyi activation cutscene");
+
+                await UniTask.DelayFrame(100);
+
+                Log.Info($"SpeedrunCutsceneSkip skipping the Ruyi restore sequence");
+                __instance.TrySkip();
+            }
+        }
+        if (__instance.transform.parent.parent.parent.name == "SimpleCutSceneFSM_軒軒到基地演出")
+        {
+            var goPath = FullPath.GetFullPath(__instance.gameObject);
+            if (goPath == "AG_S2/Room/NPCs/議會演出相關Binding/SimpleCutSceneFSM_軒軒到基地演出/FSM Animator/LogicRoot/[CutScene]")
+            {
+                Log.Info($"SpeedrunCutsceneSkip waiting 100 frames before skipping the Shuanshuan intro dialogue");
+
+                await UniTask.DelayFrame(100);
+
+                Log.Info($"SpeedrunCutsceneSkip skipping the Shuanshuan dialogue");
+                __instance.TrySkip();
+            }
+        }
+    }
+}
