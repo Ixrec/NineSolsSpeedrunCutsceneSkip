7e0202213de13ce1eca0f524b642a209b8fba038
Ixrec <ericrhitchcock@gmail.com>
2026-07-16 23:09:29 +0100

    delete build artifacts from the regular CS mod

--- Source/obj/Release/netstandard2.1/PluginInfo.cs
+++ Source/obj/Release/netstandard2.1/PluginInfo.cs
@@ -1,9 +1,9 @@
-namespace CutsceneSkip
+namespace SpeedrunCutsceneSkip
 {
     public static class PluginInfo
     {
-        public const string PLUGIN_GUID = "CutsceneSkip";
-        public const string PLUGIN_NAME = "CutsceneSkip";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_GUID = "SpeedrunCutsceneSkip";
+        public const string PLUGIN_NAME = "SpeedrunCutsceneSkip";
+        public const string PLUGIN_VERSION = "0.0.1";
     }
 }
